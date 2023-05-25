cc90d262559266a08293bbb8da7e7fa38557a4ac
Daniel Ortiz <danielortiz179499@gmail.com>
2023-05-24 20:43:52 -0500

    comentario

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import { Button, Container } from '@mui/material'
 function App() {
 	return (
 		<Container maxWidth="xl">
-			<Button variant="contained">Hola Mundo</Button>
+			<Button fullWidth variant="contained">
+				Hola Mundo
+			</Button>
 		</Container>
 	)
 }
