13481bb10172a777c64c6e3c566f6cfbf37b36fc
Daniel Ortiz <danielortiz179499@gmail.com>
2023-06-03 11:07:06 -0500

    comentario

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { HomePage } from './pages/home'
-import { LoginPage } from './pages/login'
+import { HomePage } from './pages/armoniaspa/home'
+import { LoginPage } from './pages/armoniaspa/login'
 import { RouterLayout } from './common/RouterLayout'
 export const AppRouter: React.FC<{}> = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<RouterLayout />}>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/login" element={<LoginPage />} />
+				<Route path="/armoniaspa/" element={<HomePage />} />
+				<Route path="/armoniaspa/login" element={<LoginPage />} />
 			</Route>
-            <Route path="/nose" element={<div>Hola</div>} />
+			<Route path="/nose" element={<div>Hola</div>} />
 		</Routes>
 	)
 }
