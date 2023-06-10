0f827cf65af4ba30472997e3064899618588d3cc
Daniel Ortiz <67960908+DanielOrtiz179499@users.noreply.github.com>
2023-06-10 17:19:19 -0500

    comentario

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,13 +4,12 @@ import { HomePage } from './pages/armoniaspa/home'
 import { LoginPage } from './pages/armoniaspa/login'
 import { RouterLayout } from './common/RouterLayout'
 export const AppRouter: React.FC<{}> = () => {
-	return (
-		<Routes>
-			<Route path="/" element={<RouterLayout />}>
-				<Route path="/armoniaspa/home" element={<HomePage />} />
-				<Route path="/armoniaspa/login" element={<LoginPage />} />
-			</Route>
-			<Route path="/nose" element={<div>Hola</div>} />
-		</Routes>
-	)
+  return (
+    <Routes>
+      <Route path="/" element={<RouterLayout />}>
+        <Route path="/armoniaspa/home" element={<HomePage />} />
+      </Route>
+      <Route path="/armoniaspa/login" element={<LoginPage />} />
+    </Routes>
+  )
 }
--- src/pages/armoniaspa/home/index.tsx
+++ src/pages/armoniaspa/home/index.tsx
@@ -2,11 +2,11 @@ import { Button, Container } from '@mui/material'
 import React from 'react'
 
 export const HomePage: React.FC<{}> = () => {
-	return (
-		<Container sx={{ mt: 9 }} maxWidth="xl">
-			<Button fullWidth variant="contained">
-				Home
-			</Button>
-		</Container>
-	)
+  return (
+    <Container sx={{ mt: 9 }} maxWidth="xl">
+      <Button fullWidth variant="contained">
+        Home
+      </Button>
+    </Container>
+  )
 }
