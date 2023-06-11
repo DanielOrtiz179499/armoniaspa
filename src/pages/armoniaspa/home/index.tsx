651da139ebcbbec1b86eba8bbc9589bc615f1f4f
Daniel Ortiz <67960908+DanielOrtiz179499@users.noreply.github.com>
2023-06-11 13:32:35 -0500

    comentario

--- src/pages/armoniaspa/home/index.tsx
+++ src/pages/armoniaspa/home/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Grid } from '@mui/material'
+import { Button, Container, Grid } from '@mui/material'
 import React from 'react'
 import { CardComponent, HeaderComponent } from '../../../components'
 import { characters } from '../../../api/characters'
